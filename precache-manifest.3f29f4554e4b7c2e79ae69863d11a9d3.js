acfc99b0b511502ebfdad8d1edf18933c360470b
Ken Takahashi <takahashi.ken.716@gmail.com>
2020-02-21 23:39:02 +0700

    Updates

---
+++ precache-manifest.3f29f4554e4b7c2e79ae69863d11a9d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1755dbcf62aee2d931c253b8d3bc5787",
+    "url": "/Recipe/index.html"
+  },
+  {
+    "revision": "4d1b5065df22b0ce8460",
+    "url": "/Recipe/static/css/main.357121a6.chunk.css"
+  },
+  {
+    "revision": "7ad8ea9a4ca5a282a1d6",
+    "url": "/Recipe/static/js/2.e8b2cccb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Recipe/static/js/2.e8b2cccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1b5065df22b0ce8460",
+    "url": "/Recipe/static/js/main.1a2ce3e3.chunk.js"
+  },
+  {
+    "revision": "6343d02a1c1d15df9c8d",
+    "url": "/Recipe/static/js/runtime-main.33542164.js"
+  }
+]);
